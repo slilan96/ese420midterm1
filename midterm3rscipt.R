f1fc33f7aa65795a863910d049b2ca78e801a226
cam.abdurrahman <cam.abdurrahman@gmail.com>
2018-12-18 19:18:22 -0500

    Made Evidence table into a CSV and editted. Progress on R script. Distributions, etc.

--- midterm3rscipt.R
+++ midterm3rscipt.R
@@ -9,13 +9,28 @@ NLStart("C:/Program Files/NetLogo 6.0.4/app", gui = FALSE, nl.jarname = "netlogo
 model.path <- "midtermOne.nlogo"
 # the full string for the Netlogo model
 absolute.model.path <- paste(nl.path,model.path,sep="")
+
 # loading the model into R
-NLLoadModel(absolute.model.path)
+#NLLoadModel(absolute.model.path)
 
 ###################
 
 # Part 1: We extracted H14 (Exponential Dist.) and H6: Log Normal (Log Normal Dist.)
+library(MASS)
+set.seed(101)
 
+data = read.csv(paste(nl.path, "TableCSV.csv", sep=""), header=TRUE, row.names="Persons")
+
+# without the cluster averages
+data_persons = data[-c(5, 14, 18),]
+
+H6fit <- fitdistr(data_persons$H6, "exponential")
+H6_rate <- H6fit$estimate
+
+H14fit <- fitdistr(data_persons$H14, "log-normal")
+H14fit$loglik
+H14_mean <- H14fit$estimate[1]
+H14_sd <- H14fit$estimate[2]
 
 # Part 2:
 
@@ -25,9 +40,48 @@ NLLoadModel(absolute.model.path)
 #     constant) and run the model.
 ##
 
+# H6: Influence of a Close One Losing a Bet
+# H14: The Decay of Affects of Violation of Principles
+
+# loading the model into R
+NLLoadModel(absolute.model.path)
+
+# rexp(n times to sample, lambda rate of arrival)
+H6_sample <- rexp(1, H6_rate)
+H14_sample <- rlnorm(1, H14_mean, H14_sd)
+
+# ##TODO Think about whether we should be separetely coming up with values
+# For each cluster...
+
+# Running model combinatorially with these samples
+NLCommand(sprintf("ask turtles [ set agent-winning-losing-influence %s]", H6_sample))
+NLCommand(sprintf("ask turtles [ set agent-shame-decay %s]", H14_sample))
+NLCommand("setup")
+NLDoCommandWhile("number-of-bets-made < 100", "go")
+ret <- NLReport("sum [agent-shame] of turtles")
+ret
+
+#timedata[[i]] <- NLGetAgentSet(c("who", "xcor", "ycor", "agent-shame","bankrupt"), "turtles")}
+#timedata
+#for (i in 1:10){write.csv(timedata[i], file="C:/Users/Abdurrahman/Desktop/UPenn20182019Senior/ESE420/ese420midterm1/log.csv")}
+
 ##
 # (b) Sample from each PDF and plot to show that we are sampling correctly.
 ##
+
+# number of times to sample
+n <- 100
+
+# rexp(n times to sample, lambda rate of arrival)
+H6_sampling <- rexp(n, H6_rate)
+H14_sampling <- rlnorm(n, H14_mean, H14_sd)
+
+# the exponential distribution plot of H6
+plot(density(H6_sampling))
+
+# the log normal distribution plot of H14
+plot(density(H14_sampling))
+
 
 ##
 # (c) Sample the results of the model multiple times at the same numbers to show that
